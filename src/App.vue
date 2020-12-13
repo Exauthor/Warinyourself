489f9eeb43a7311b23a94924feaf9d2cfe0c7a7e
Warinyourself <dmitriysuvorov1997@gmail.com>
2020-12-14 00:37:39 +0300

    Update title

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="home">
-    <div class="title" ref="scene" data-scalar-x="25" data-scalar-y="25">
-      <video class="title-video" data-depth="0.1" autoplay loop muted>
-        <source :src="require('./assets/eruption.mp4')" type="video/mp4" />
+    <div class="title" :key="activeTitle.video" ref="scene">
+      <video
+        class="title-video"
+        autoplay
+        loop
+        muted
+        :style="activeTitle.videoStyle || {}"
+      >
+        <source
+          :src="require(`./assets/${activeTitle.video}.mp4`)"
+          type="video/mp4"
+        />
         Your browser does not support the video tag.
       </video>
       <svg
@@ -13,13 +22,14 @@
         <defs>
           <clipPath id="cut-off-bottom">
             <text
-              :y="height / 2.2"
+              :y="height * (activeTitle.offsetY || 0.65)"
               class="title-text"
               fill="white"
               xml:space="preserve"
             >
-              <tspan x="0" text-anchor="middle">MY</tspan>
-              <tspan x="0" :y="height / 1.2" text-anchor="middle">WORLD</tspan>
+              <tspan text-anchor="middle" :style="activeTitle.style">
+                {{ activeTitle.text }}
+              </tspan>
             </text>
           </clipPath>
         </defs>
@@ -31,13 +41,109 @@
 <script lang="ts">
 import { onMounted, ref } from "vue";
 
-import Parallax from "parallax-js";
-
 export default {
   setup() {
-    const scene = ref(null);
+    const scene = ref<any>(null);
     const height = ref(0);
     const width = ref(0);
+    const titles = [
+      {
+        text: "my",
+        video: "lightning",
+        offsetY: 0.87,
+        style: {
+          fontSize: "1em"
+        },
+        videoStyle: {
+          width: "4ch"
+        }
+      },
+      {
+        text: "world",
+        video: "rocket",
+        offsetY: 0.67,
+        style: {
+          fontSize: "0.85em"
+        },
+        videoStyle: {
+          width: "9ch"
+        }
+      },
+      {
+        text: "is",
+        video: "smokeOut",
+        offsetY: 1,
+        style: {
+          fontSize: "1em"
+        },
+        videoStyle: {
+          width: "3ch",
+          top: "0",
+          left: "0px"
+        }
+      },
+      {
+        text: "stress",
+        video: "nuclearBomb",
+        offsetY: 0.71,
+        style: {
+          fontSize: "0.85em"
+        },
+        videoStyle: {
+          width: "7ch",
+          top: "-0.125em"
+        }
+      },
+      {
+        text: "pain",
+        video: "eruption",
+        offsetY: 0.8,
+        style: {
+          fontSize: "1em"
+        },
+        videoStyle: {
+          width: "5.1ch",
+          top: "0.14em"
+        }
+      },
+      {
+        text: "hunger",
+        video: "tiger",
+        offsetY: 0.7,
+        style: {
+          fontSize: "0.85em"
+        },
+        videoStyle: {
+          width: "7.5ch",
+          top: "0.13em"
+        }
+      },
+      {
+        text: "i am",
+        video: "tornado",
+        offsetY: 0.78,
+        style: {
+          fontSize: "0.92em"
+        },
+        videoStyle: {
+          width: "5ch",
+          top: "-0.35em"
+        }
+      },
+      {
+        text: "warinyourself",
+        video: "eruptionTimelapse",
+        offsetY: 0.63,
+        style: {
+          fontSize: "0.5em"
+        },
+        videoStyle: {
+          width: "10ch",
+          top: "0.11em"
+        }
+      }
+    ];
+    const activeTitle = ref(titles[0]);
 
     onMounted(() => {
       if (scene.value != null) {
@@ -45,15 +151,40 @@ export default {
         [height.value, width.value] = [bound.height, bound.width];
 
         window.addEventListener("resize", () => {
-          const bound = scene.value.getBoundingClientRect();
-          [height.value, width.value] = [bound.height, bound.width];
+          setTimeout(() => {
+            const bound = scene.value.getBoundingClientRect();
+            [height.value, width.value] = [bound.height, bound.width];
+          }, 100);
         });
 
-        const parallax = new Parallax(scene.value);
+        const updateTitle = () =>
+          setTimeout(() => {
+            let index = titles.findIndex(
+              title => title.text === activeTitle.value.text
+            );
+            if (index === undefined) return;
+
+            const isLast = index === titles.length - 1;
+
+            if (!isLast) {
+              activeTitle.value = titles[++index];
+
+              setTimeout(() => {
+                const bound = scene.value.getBoundingClientRect();
+                [height.value, width.value] = [bound?.height, bound.width];
+
+                console.log({ height: height.value, width: width.value });
+              });
+
+              updateTitle();
+            }
+          }, 2000);
+
+        updateTitle();
       }
     });
 
-    return { scene, height, width };
+    return { scene, height, width, activeTitle };
   }
 };
 </script>
